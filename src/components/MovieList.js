b5efbef6ac3291a92442d52c42eeeda4eb3128ef
GisnetzEU <gismontero.mail@gmail.com>
2022-07-10 22:02:58 +0200

    Movie List with new url

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -7,8 +7,9 @@ const MovieList = (props) => {
 		<div className='box'>
 			{props.movies.map((movie, index) => (
 				<div key={movie.movieUUID} className='image-container'>
-					<img src={`http://localhost:8083/posters/${movie.posterPath}`} alt='movie'  width='150' onClick={() => {
-						props.opendetail(movie.movieUUID) 						
+					<br></br>
+					<img src={`https://filmstreamercontentmanager.herokuapp.com/images/${movie.posterPath}`} alt='movie' width='150' onClick={() => {
+						props.opendetail(movie.movieUUID)
 					}}></img>
 					<p onClick={() => {
 						props.opendetail(movie.movieUUID)
